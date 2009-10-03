03f000440a68f087f9080f827c76a539266ba681
Tim Weber <scy@scytale.name>
2009-10-03 17:35:13 +0200

    Add config.mk changes suggested by Gentoo.

--- config.mk
+++ config.mk
@@ -10,9 +10,9 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-# Xinerama, comment if you don't want it
-XINERAMALIBS = -L${X11LIB} -lXinerama
-XINERAMAFLAGS = -DXINERAMA
+# Xinerama, set the vars to "" if you don't want it
+XINERAMALIBS ?= -L${X11LIB} -lXinerama
+XINERAMAFLAGS ?= -DXINERAMA
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC}
@@ -20,8 +20,8 @@ LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${XINERAMALIBS}
 
 # flags
 CPPFLAGS = -D_BSD_SOURCE -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
-LDFLAGS = -s ${LIBS}
+CFLAGS += -std=c99 -pedantic -Wall -g ${INCS} ${CPPFLAGS}
+LDFLAGS += -g ${LIBS}
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
